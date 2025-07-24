c0775817d24be55ec6d93ae7af6f5673c045284d
Izzat Amir <izzatamir09@gmail.com>
2025-07-24 12:17:27 +0800

    Navbar Update, Changes of Shared Components

--- user/public/maintenance-mode.php
+++ user/public/maintenance-mode.php
@@ -125,63 +125,8 @@
 				</div>
 			</div>
 
-			<footer id="footer">
-				<div class="container">
-					<div class="row py-5">
-						<div class="col-md-4 d-flex justify-content-center justify-content-md-start mb-4 mb-lg-0">
-							<a href="index.html" class="logo pe-0 pe-lg-3 ps-3 ps-md-0">
-								<img alt="Porto Website Template" src="img/logo-footer.png" height="32">
-							</a>
-						</div>
-						<div class="col-md-8 d-flex justify-content-center justify-content-md-end mb-4 mb-lg-0">
-							<div class="row">
-								<div class="col-md-6 mb-3 mb-md-0">
-									<div class="ms-3 text-center text-md-end">
-										<h5 class="text-3 mb-0 text-color-light">NEW YORK</h5>
-										<p class="text-4 mb-0"><i class="fab fa-whatsapp text-color-primary top-1 p-relative"></i><span class="ps-1"><a href="tel:012345679" class="text-color-hover-light">(123) 465-7890</a></span></p>            
-									</div>
-								</div>
-								<div class="col-md-6">
-									<div class="ms-3 text-center text-md-end">
-										<h5 class="text-3 mb-0 text-color-light">LOS ANGELES</h5>
-										<p class="text-4 mb-0"><i class="fab fa-whatsapp text-color-primary top-1 p-relative"></i><span class="ps-1"><a href="tel:012345679" class="text-color-hover-light">(123) 465-7890</a></span></p>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="footer-copyright footer-copyright-style-2">
-					<div class="container py-2">
-						<div class="row py-4">
-							<div class="col-md-4 d-flex align-items-center justify-content-center justify-content-md-start mb-2 mb-lg-0">
-								<p>© Copyright 2024. All Rights Reserved.</p>
-							</div>
-							<div class="col-md-8 d-flex align-items-center justify-content-center justify-content-md-end mb-4 mb-lg-0">
-								<p><i class="far fa-envelope text-color-primary top-1 p-relative"></i><a href="mailto:mail@example.com" class="opacity-7 ps-1">mail@example.com</a></p>
-								<ul class="footer-social-icons social-icons social-icons-clean social-icons-icon-light ms-3">
-									<li class="social-icons-facebook"><a href="http://www.facebook.com/" target="_blank" title="Facebook"><i class="fab fa-facebook-f text-2"></i></a></li>
-									<li class="social-icons-x"><a href="http://www.x.com/" target="_blank" title="X"><i class="fab fa-x-twitter text-2"></i></a></li>
-									<li class="social-icons-linkedin"><a href="http://www.linkedin.com/" target="_blank" title="Linkedin"><i class="fab fa-linkedin-in text-2"></i></a></li>
-								</ul>
-							</div>
-						</div>
-					</div>
-				</div>
-			</footer>
-		</div>
+<?php
 
-		<!-- Vendor -->
-		<script src="vendor/plugins/js/plugins.min.js"></script>
+	require_once('../include/footer.php');
 
-		<!-- Theme Base, Components and Settings -->
-		<script src="js/theme.js"></script>
-
-		<!-- Theme Custom -->
-		<script src="js/custom.js"></script>
-
-		<!-- Theme Initialization Files -->
-		<script src="js/theme.init.js"></script>
-
-	</body>
-</html>
+?>
