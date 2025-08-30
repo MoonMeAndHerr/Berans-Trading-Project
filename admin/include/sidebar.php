cf055bb959fd70f75f472438576fb306a53dfa5c
Izzat Amir <izzatamir09@gmail.com>
2025-08-31 06:43:34 +0800

    Update admin sidebar and clean debug log

--- admin/include/sidebar.php
+++ admin/include/sidebar.php
@@ -16,7 +16,7 @@
                     </span>
                 </a>
                 <!-- Light Logo-->
-                <a href="index.html" class="logo logo-light">
+                <a href="index" class="logo logo-light">
                     <span class="logo-sm">
                         <img src="assets/images/logo-sm.png" alt="" height="22">
                     </span>
