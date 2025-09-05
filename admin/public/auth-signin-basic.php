d383bd346406c4e6f9172d25ab584ceced0df06c
iTzBGGaming <45941903+iTzBGGaming@users.noreply.github.com>
2025-09-05 19:34:41 +0800

    Fix

--- admin/public/auth-signin-basic.php
+++ admin/public/auth-signin-basic.php
@@ -51,7 +51,7 @@
                                         <div class="text-center mt-sm-5 mb-4 text-white-50">
                                             <div>
                                                 <a href="index.html" class="d-inline-block auth-logo">
-                                                    <img src="assets/images/logo-light.png" alt="" height="20">
+                                                    <img src="/beranstrading/assets/images/beranslogo.png" alt="" height="70">
                                                 </a>
                                             </div>
                                             <p class="mt-3 fs-15 fw-medium">BeransTrading, Your One Stop Supplies</p>
@@ -118,7 +118,7 @@
                     <div class="row">
                         <div class="col-lg-12">
                             <div class="text-center">
-                                <p class="mb-0 text-muted">&copy; <script>document.write(new Date().getFullYear())</script> BeransTrading . Crafted with <i class="mdi mdi-heart text-danger"></i> by Themesbrand</p>
+                                <p class="mb-0 text-muted">&copy; <script>document.write(new Date().getFullYear())</script> BeransTrading</p>
                             </div>
                         </div>
                     </div>
